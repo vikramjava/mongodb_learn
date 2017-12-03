2b652e4201115265ec6b7daa0046d6f2ed9fb152
Vikram Java <vikram.g.j.coding@gmail.com>
2017-12-02 16:13:48 -0800

    Added tests to update and remove subdocuments

--- prod/users/test/subdocument_test.js
+++ prod/users/test/subdocument_test.js
@@ -15,4 +15,45 @@ describe('Subdocuments', () => {
       done();
     })
   });
+
+  it("Can add a subdocument to an already present record", (done) => {
+    const joe = new User({
+      name : 'Joe',
+      posts : [ ]
+    });
+
+    joe.save()
+      .then(() => User.findOne({ name : "Joe"}))
+      .then((user) => {
+        user.posts.push({ title : "New Post" });
+        return user.save();
+      })
+      .then(() => User.findOne({ name : "Joe" }))
+      .then((user) => {
+        assert(user.posts[0].title === "New Post");
+        done();
+      });
+  });
+
+  it("ca =n remove an exisiting subdocument", (done) => {
+    const joe = new User ({
+      name : "Joe",
+      posts : [{ title : 'New Title'}]
+    })
+
+    joe.save()
+      .then(() => User.findOne({ name : "Joe"}))
+      .then((user) => {
+        const post = user.posts[0];
+        // This call does not remove the document from the DB.
+        // Still need to call save to remove this subdocument.
+        post.remove();
+        return user.save();
+      })
+      .then(() => User.findOne({ name : "Joe" }))
+      .then((user) => {
+        assert(user.posts.length == 0);
+        done();
+      });
+  });
 });
